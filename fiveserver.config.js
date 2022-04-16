beb7262df52d4cc28295288d5538f3214f7fc9b5
Kranarong Kuanhat <mr.kranarong@gmail.com>
2022-04-17 00:56:54 +0700

    Page layout + Sidebar + Decorations
    Done
    - Page Layout
    - Sidebar (Picture, Information)
    - Decorations
    
    To do
    - Main Content (2 tabs, History and Project)
    - Mobile Responsive

---
+++ fiveserver.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  highlight: true, // enable highlight feature
+  injectBody: true, // enable instant update
+  injectCss: false, // disable injecting css
+  navigate: true, // enable auto-navigation
+};
